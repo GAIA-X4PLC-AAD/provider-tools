482b3f3828a51ffae83b01e968b5a9f076ec88b0
Mirco <Mirco.Nierenz@triangraphics.de>
2024-10-08 19:19:38 +0200

    support naming mask in structure_creator

--- structure_creator/main.py
+++ structure_creator/main.py
@@ -15,18 +15,51 @@ extensions = {
     'xml': 'Data'
 }
 
-folder_names = {
-    'Asset' : 'data',
-    'Data' : 'data',
-    'Document' : 'documentation', 
-    'License' : 'documentation', 
-    'Metadata' : 'metadata',
-    'Service' : 'services',
-    'Validation' : 'validation', 
-    'Image' : 'visualization', 
-    'Routing' : 'visualization',
-    'Video' : 'visualization',
-    '3DPreview' : 'visualization'
+type_data = {
+    'Asset' : {
+        'folder' : 'data',
+        'mask' : '{asset}'
+    },
+    'Data' : {
+        'folder' : 'data',
+        'mask' : '{asset}'
+    },
+    'Document' : {
+        'folder' : 'documentation',
+        'mask' : '{asset}_technicalDocumentation'
+    }, 
+    'License' : {
+        'folder' : 'documentation',
+        'mask' : 'LICENSE'
+    },
+    'Metadata' : {
+        'folder' : 'metadata',
+        'mask' : 'domain_metadata'
+    },
+    'Service' : {
+        'folder' : 'services',
+        'mask' : '{asset}_extSearch'
+    },
+    'Validation' : {
+        'folder' : 'validation',
+        'mask' : 'qcReport'
+    },
+    'Image' : {
+        'folder' : 'visualization',
+        'mask' : '{asset}'
+    },
+    'Routing' : {
+        'folder' : 'visualization',
+        'mask' : 'roadNetwork'
+    },
+    'Video' : {
+        'folder' : 'visualization',
+        'mask' : '{asset}'
+    },
+    '3DPreview' : {
+        'folder' : 'visualization',
+        'mask' : 'detailRoadNetwork'
+    }
 }
 
 def get_data_typ(file: Path)-> str:
@@ -72,6 +105,28 @@ def register_data(data: dict, user_data: dict, path: Path, role: str, category: 
             data[category] = files
 
 
+def createFileName(filename: Path, asset_name: Path, type : str, index : int) -> Path:
+    basename = str(filename.stem)  # Name without extension
+    if type in type_data:
+        mask = type_data[type]['mask']
+    else:
+        print (f'type {type} not found in type_data')
+        exit(1)
+    basename = mask.replace(r"{file}", basename)
+    basename = mask.replace(r"{asset}", asset_name)
+    extension = filename.suffix
+
+    if type == 'Image':
+        if index == 0:
+            filename_new = f"{basename}_eyecatcher{extension}"
+        else:
+            number = str(index).zfill(2)
+            filename_new = f"{basename}_impression-{number}{extension}"
+    else:
+        filename_new = f"{basename}{extension}"
+    return Path(filename_new)
+
+
 def main():
     parser = argparse.ArgumentParser(prog='main.py', description='the folder structure is completed from the user info and a metadata table is created for the manifest')   
     parser.add_argument('filename', help='filename of json file from frontend.')
@@ -98,18 +153,43 @@ def main():
     # read json
     with open(user_input_file, 'r') as file:
         user_data = json.load(file)
-    
+
+    # get asset name
+    for file in user_data:
+        if file['type'] == 'Asset':
+            asset_name = Path(file['filename'])
+            asset_name = asset_name.stem
+            break
+    if not asset_name:
+        print (f'no asset found in {file}')
+        exit(1)
+
     # copy files
     upload_folder = user_input_file.parent
+    indexImage = 0
     for file in user_data:
         filename = Path(file['filename'])
-        sub_folder = folder_names[file['type']]
-        name = filename.name
+        # get sub folder
+        type = file['type']
+        if type in type_data:
+            sub_folder = type_data[type]['folder']
+        else:
+            print (f'type {type} not found in type_data')
+            exit(1)
+        # get dest name
+        dest_name = filename.name
+        dest_name = createFileName(Path(dest_name), asset_name, type, indexImage)        
+        if type == 'Image':
+            indexImage = indexImage + 1 # incrase image index for image mask
+
+        # destination filename
         dest = data_path / sub_folder
         if not dest.exists():
             dest.mkdir()
-        dest = dest /  name
+        dest = dest /  dest_name        
+        # source filename
         source = upload_folder / filename
+        # copy
         shutil.copy(source, dest)
 
     # create json file for jsonLD creator

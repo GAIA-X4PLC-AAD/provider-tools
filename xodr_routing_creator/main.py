fa619f272b3479486cd3f02aef549a5ece8f051c
Mirco <Mirco.Nierenz@triangraphics.de>
2024-10-09 09:08:56 +0200

    improve xodr_routing_creator - create also bounding box polygon

--- xodr_routing_creator/main.py
+++ xodr_routing_creator/main.py
@@ -13,7 +13,16 @@ class Vec2:
         self.x = x
         self.y = y
 
-# Function to parse the XML file and extract coordinates
+
+class BoundingBox:
+    def __init__(self, xMin, yMin, xMax, yMax):
+        self.xMin = xMin
+        self.yMin = yMin
+        self.xMax = xMax
+        self.yMax = yMax
+
+
+# function to parse the XML file and extract coordinates
 def parse_xml(file_path):
     tree = ET.parse(file_path)
     root = tree.getroot()
@@ -40,12 +49,14 @@ def parse_xml(file_path):
         lines.append(coordinates)
     return proj4_str, offset, lines
 
+
 def calculate_end_position(start_pos : Vec2, heading, length):
     end_x = start_pos.x + math.cos(heading) * length
     end_y = start_pos.y + math.sin(heading) * length
     return Vec2(end_x, end_y)
 
-# Function to reproject the coordinates
+
+# function to reproject the coordinates
 def reproject(lines, offset, transformer):
     transformed_lines = []
     for line in lines:
@@ -64,27 +75,42 @@ def reproject(lines, offset, transformer):
     return transformed_lines
 
 
-# Function to create and write KML lines
-def create_kml(lines, output_file):
+# Function to create and write KML elements
+def create_kml(elements: list, output_file: Path, isPolygon: bool):
     kml = simplekml.Kml()
-    for line in lines:
-        linestring = kml.newlinestring(name="Line")
-        linestring.coords = line
+    for element in elements:
+        if isPolygon:
+            datastring = kml.newpolygon(name="Box")
+            datastring.outerboundaryis = element
+        else:
+            datastring = kml.newlinestring(name="Line")
+            datastring.coords = element
 
     kml.save(output_file)
 
-# Function to create and write a GeoJson lines
-def create_geojson(lines, output_file):
+
+# Function to create and write a GeoJson elements
+def create_geojson(elements: list, output_file: Path, isPolygon: bool):
     features = []
-    for line in lines:
-        feature = {
+    for element in elements:
+        if isPolygon:
+            feature = {
             "type": "Feature",
             "geometry": {
-                "type": "LineString",
-                "coordinates": [(lon, lat) for lon, lat in line]
+                "type": "MultiPolygon",
+                "coordinates": [(lon, lat) for lon, lat in element]
             },
             "properties": {}
         }
+        else:
+            feature = {
+                "type": "Feature",
+                "geometry": {
+                    "type": "LineString",
+                    "coordinates": [(lon, lat) for lon, lat in element]
+                },
+                "properties": {}
+            }
         features.append(feature)
 
     geojson = {
@@ -96,11 +122,30 @@ def create_geojson(lines, output_file):
     with open(output_file, 'w') as f:
         json.dump(geojson, f, indent=2)
 
+
+# function to create bounding box from point list
+def create_bounding_box(elements: list) -> BoundingBox :        
+    x_coords = []
+    y_coords = []
+    for element in elements:
+        for coord in element:
+            x_coords.append(coord[0])
+            y_coords.append(coord[1])
+
+    box = BoundingBox
+    box.xMin = min(x_coords)
+    box.yMin = min(y_coords)
+    box.xMax = max(x_coords)
+    box.yMax = max(y_coords)
+
+    return box
+
+
 def main():
     parser = argparse.ArgumentParser(prog='main.py', description='creates routing files (as geojson or kml) on OpenDRIVE files.')   
     parser.add_argument('filename', help='filename of OpenDRIVE file.')
     parser.add_argument('-out', type=str,help='filename of exported geo file.')
-    parser.add_argument('-format', choices=['kml', 'geojson'], default='geojson', help='output format for geo file.')
+    parser.add_argument('-box', type=str,help='filename for boundingbox geo file.')
     args = parser.parse_args()
 
     xodr_file = Path(args.filename)
@@ -112,7 +157,7 @@ def main():
     if not output_file.parent.exists():
         output_file.parent.mkdir()
 
-    format = args.format
+    extension = output_file.suffix
 
     # Parse the XML file and extract coordinates
     in_proj, offset, lines = parse_xml(xodr_file)
@@ -129,11 +174,27 @@ def main():
     # Reproject the coordinates
     transformed_lines= reproject(lines, offset, transformer_proj_to_wgs84)
 
+    output_file_box = args.box
+    if output_file_box:
+        box = create_bounding_box(transformed_lines)
+        coordinates = []
+        coordinates.append((box.xMin,box.yMin))
+        coordinates.append((box.xMax,box.yMin))
+        coordinates.append((box.xMax,box.yMax))
+        coordinates.append((box.xMin,box.yMax))
+        coordinates.append((box.xMin,box.yMin))
+        boxes = []
+        boxes.append(coordinates)
+        if extension == "geojson":
+            create_geojson(boxes, output_file_box, True)
+        else:
+            create_kml(boxes, output_file_box, True)
+
     # Create the KML line
-    if format == "geojson":
-        create_geojson(transformed_lines, output_file)
+    if extension == "geojson":
+        create_geojson(transformed_lines, output_file, False)
     else:
-        create_kml(transformed_lines, output_file)
+        create_kml(transformed_lines, output_file, False)
 
     print(f"KML file created: {output_file}")
 

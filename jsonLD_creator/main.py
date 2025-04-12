2dc2d158498dd2686d43058f928c68916a85ce9d
Mirco Nierenz <mirco.nierenz@triangraphics.de>
2025-04-12 15:02:44 +0200

    remove old code

--- jsonLD_creator/main.py
+++ jsonLD_creator/main.py
@@ -49,101 +49,6 @@ def datetime_handler(x):
     if isinstance(x, datetime):
         return x.isoformat()
     raise TypeError("Unknown type")
-
-"""
-
-dataTypeMap = {
-    'xsd:string': { 'type' : 'str', 'default' : ''},
-    'xsd:boolean': { 'type' : 'bool', 'default' : 'false'},
-    'xsd:datetime.datetime': { 'type' : 'datetime.datetime', 'default' : '01-01-2000'},
-    'xsd:dateTime': { 'type' : 'datetime.datetime', 'default' : '01-01-2000'},
-    'xsd:integer': { 'type' : 'int', 'default' : '0'},
-    'xsd:int': { 'type' : 'int', 'default' : '0'},
-    'xsd:unsignedInt':  { 'type' : 'int', 'default' : '0'},
-    'xsd:float': { 'type' : 'float', 'default' : '0.0'},
-    'xsd:anyURI': { 'type' : 'str', 'default' : ''}
-}   
-
-def check_data_type(value, type_string: str, key: str) ->bool:
-    if type_string in dataTypeMap:
-        
-        type_value = str(type(value))
-        #suported_type = f'<class \'{dataTypeMap[type_string]['type']}\'>'
-        
-        # extract 'type' from dataTypeMap before using it in the f-string
-        expected_type = dataTypeMap[type_string]['type']
-        suported_type = f'<class "{expected_type}">'
-        if type_value == suported_type:
-            return True
-        elif isinstance(value, list):
-            return True
-        else:
-            logging.error(f'data type not match {type_value} != {suported_type} for {key}')
-    else:
-        logging.error(f'unsupported data type {type_string} for {key}')
-    return False
-
-
-def check_value_type(value: str, data_type: str) -> any:
-    if data_type in dataTypeMap:
-        if dataTypeMap[data_type]['type'] == 'str':
-            return value
-        elif dataTypeMap[data_type]['type'] == 'int':
-            try:
-                return int(value)
-            except ValueError:
-                return None
-        elif dataTypeMap[data_type]['type'] == 'float':
-            try:
-                return float(value)
-            except ValueError:
-                return None
-        elif dataTypeMap[data_type]['type'] == 'datetime.datetime':
-            # TODO doesnt work!
-            try:
-                data_time = datetime.strptime(value, "%d-%m-%Y")
-                logging.debug(type(data_time))
-                return data_time
-            except ValueError:
-                return None
-        else:
-            logging.error(f'unsupported data type {dataTypeMap[data_type]}')
-            return None
-    else: 
-        logging.error(f'unsupported data type {data_type}')
-        return None
-
-
-def replace_namespace(string: str, namespace: dict) -> str:
-    for prefix, name in namespace.items():
-        if name in string:
-            return string.replace(name, f'{prefix}:')
-    return string
-
-def camel_case_to_lower(string):
-    return string[0].lower() + string[1:]
-
-def convert_url_to_namespace(url, as_node_name=False, namespace=None):
-    # parse url
-    parsed_url = urlparse(url)
-
-    # split the path into segments and remove empty entries
-    segments = [segment for segment in parsed_url.path.split('/') if segment]
-    if segments:
-        subelement = segments[0]
-
-        if as_node_name:            
-            lastElement = subelement
-        else:
-            lastElement = subelement.capitalize()
-
-        if namespace:
-            return f"{namespace}:{lastElement}"
-        else:
-            return f"{subelement}:{lastElement}"
-    
-    return ""    
-"""
     
 # check if value is greater/smaller then value
 def check_min_max(shacl_data, name: str, compare_value: int, greater : bool):
@@ -190,24 +95,6 @@ def is_list_property(shacl_data):
     return check
 
 
-# from 'https://github.com/GAIA-X4PLC-AAD/ontology-management-base/tree/main/hdmap/HdMapShape'
-# to hdmap::HdMap
-# or from https://ontologies.envited-x.net/manifest/v4/ontology#LinkShape
-# to manifest::Manifest
-def convert_path_to_namespace_old(path, as_node_name=False, namespace=None):
-    logging.info(f'path to namespace {path}')
-    parts = path.split('/')
-    last_parts = parts[-2:]
-    last_parts[-1] = last_parts[-1].replace('Shape', '')
-    if as_node_name:
-        last_parts[-1] = last_parts[-1][0].lower()+ last_parts[-1][1:]
-    if namespace:
-        last_parts[0] = namespace
-    separator = ':'
-    namespace = separator.join(last_parts)
-    return namespace
-
-
 # get named value
 def get_value(name, values, check_lower_case : bool):
     for key, data in values.items():
@@ -245,255 +132,6 @@ def get_node_data(values) -> Tuple[str, list]:
         return path_data, node_paths
     return path_data, None
 
-"""
-def get_property_value(name, values):
-    for key, data in values.items():
-        key_str = key.split("#")[-1]
-        if name == key_str:
-            return data        
-    return None        
-
-
-def get_data_from_metadata(path, meta_data):
-    if meta_data is not None:
-        if path in meta_data:           
-                return meta_data[path]
-    return None
-
-
-def get_namespace_and_name(namespace, name):
-    return f'{namespace}:{name}'
-
-
-def is_in_namespace(name, namespace):
-    if f'{namespace}:' in name:
-        return True
-    return False
-
-def create_group_name(node_path: str, forType: bool):
-    last_part = node_path.rstrip('/').split('/')[-1]
-    if last_part.endswith("Shape"):
-        last_part = last_part[:-len("Shape")]
-    if forType:
-        return last_part
-    else:
-        return last_part[0].lower() + last_part[1:]
-
-
-def get_schema_from_node_path(node_path: str):
-    parts = node_path.split('/')
-    return parts[-2]
-
-
-# create group with type, e.g
-#   "hdmap:georeference": {
-#    "@type": "georeference:Georeference",
-def create_group(as_list, group_name, node_path, parent_group, level, register=True):
-
-    # create group
-    if as_list:
-        group = list()
-        logging.debug(f'{" " * level * 3}add list {group_name}')  
-
-    else:
-        group = dict()
-        group['@type'] = convert_url_to_namespace(node_path, False, get_schema_from_node_path(node_path))
-        logging.debug(f'{" " * level * 3}add dict {group_name}')  
-
-    if register:
-        if isinstance(parent_group, list):
-            parent_group.append(group)
-        else:
-            parent_group[group_name] = group
-    return group
-
-
-def find_node_path_in_shacles(node_path):
-    name = convert_url_to_namespace(node_path)
-    namespace, namespace_name = get_namespace(name)
-    if namespace in config.SHACLS:
-        shacl_graph = config.SHACLS[namespace]
-        prefixes = shacl_graph['prefixes']
-        shacl_dict = shacl_graph['dict']
-        return namespace, shacl_dict, prefixes
-    return None
-
-def fill_content(node, node_path, node_path_name, schema_name, group, shacl_dict, prefixes, meta_data, level):
-
-    # create properties group
-    prop_group = create_group(False, node_path_name, node_path, group, level, False)
-
-    # loop properties
-    for properties in node:                     
-        prop_name = get_value('name', properties, False)
-        prop_node = get_value('node', properties, False, True)        
-        if prop_node: # property has sub structure
-            shacl_data = find_node_path_in_shacles(prop_node)
-            if shacl_data is None:
-                continue
-            namespace_add, shacl_dict_add, prefixes_add = shacl_data
-            if prop_node in shacl_dict_add:
-                node = shacl_dict_add[prop_node]
-                node_path_name = get_value('path', properties, False)
-                node_path_name = convert_url_to_namespace(node_path_name, False, schema_name)
-                isList_sub = is_list_property(properties)
-                if node_path_name not in meta_data:
-                    continue
-                # create sub group and fill sub content
-                meta_data_sub = meta_data[node_path_name]
-                group_to_add = group
-                if isinstance(group_to_add, list):
-                    group_to_add = prop_group
-                group_sub = create_group(isList_sub, node_path_name, prop_node, group_to_add, level)
-                if isinstance(meta_data_sub, list):                    
-                    for meta_sub_element in meta_data_sub:
-                        name = convert_url_to_namespace(prop_node)
-                        namespace, namespace_name = get_namespace(name)
-                        fill_content(node, prop_node, node_path_name, namespace, group_sub, shacl_dict, prefixes, meta_sub_element, level+1)
-                else:
-                    fill_content(node, prop_node, node_path_name, schema_name, group_sub, shacl_dict, prefixes, meta_data_sub, level+1)
-
-            else:
-                if is_required_property(properties):
-                    name = convert_url_to_namespace(prop_node)
-                    namespace, namespace_name = get_namespace(name)
-                    fill_properties_in_other_namespace(properties, prop_node, namespace, meta_data, level)
-            continue
-
-        if not create_property_old(properties, prefixes, meta_data, schema_name, group, prop_group, level):
-            continue
-
-
-    
-    if isinstance(group, list):
-        group.append(prop_group)
-
-def create_property_old(properties, prefixes, meta_data, schema_name, group, prop_group, level):
-    # property type
-    prop_type = get_value('datatype', properties, False)
-    if not prop_type:
-        return False
-
-    # create property and get data type
-    data_type = None
-    isList_prop = is_list_property(properties)
-    if isList_prop:
-        property = list()           
-    else:
-        property = dict()
-        data_type = replace_namespace(prop_type, prefixes)
-        if data_type == "xsd:anyURI":
-            property = dict()       
-
-    # prop_path
-    prop_path = get_value('path', properties, False)
-    prop_path = replace_namespace(prop_path, prefixes)        
-    # set value
-    data_from_metadata = get_data_from_metadata(prop_path, meta_data)
-    if data_from_metadata is not None: # has data
-        if isList_prop:
-            for data_value in data_from_metadata:
-                property.append(data_value)
-        elif type(property) == dict:
-            property['@value'] = str(data_from_metadata)
-        else:
-            property = data_from_metadata
-    elif is_required_property(properties): # is required
-        data_value = check_value_type(dataTypeMap[data_type]['default'], data_type)
-        if isList_prop:
-            property.append(data_value)    
-        elif type(property) == dict:
-            property['@value'] = str(data_value)
-        else:
-            property = data_value
-    elif is_in_namespace(prop_path, schema_name): # not filled -> ignore
-        #logging.warning(f'{prop_path} not found!!')
-        return False
-    else:
-        return False   
-    
-    sh_in = get_property_value('in', properties)
-    if data_type and not sh_in: # no type for enums!
-        property['@type'] = data_type    
-
-        # register
-    if isinstance(group, list):
-        logging.debug(f'{" "  * level * 3}add prop {prop_path}')
-        prop_group[prop_path] = property
-    else:
-        logging.debug(f'{" " * level * 3}add prop {prop_path}')
-        group[prop_path] = property    
-
-    return True 
-
-def fill_properties_in_other_namespace(node_properties, node_path, schema_namespace, meta_data, level):
-    # then switch to specific shacle
-    shacl_data = find_node_path_in_shacles(node_path)
-    if shacl_data is None:
-        return
-    namespace, shacl_dict_add, prefixes_add = shacl_data
-    group_name = convert_url_to_namespace(node_path, True, schema_namespace)
-    if group_name in meta_data:
-        # switch to subgraph
-        meta_data_sub = meta_data[group_name]
-        group_dict = create_group(False, group_name, node_path, config.JSON_OUT, level)
-        schema_shape_add = shacl_dict_add[node_path]
-        # loop properties in specific shacle
-        for node_properties_add in schema_shape_add:
-            node_path_add = get_value('node', node_properties_add, False)
-            if node_path_add in shacl_dict_add:
-                # check if node in meta data
-                node_path_name = get_value('path', node_properties_add, False)
-                node_path_name = convert_url_to_namespace(node_path_name, True, namespace)
-                if node_path_name in meta_data_sub:
-                    # create sub group and fill sub content
-                    node = shacl_dict_add[node_path_add]
-                    as_list_add = is_list_property(node_properties_add)
-                    meta_data_sub_sub = meta_data_sub[node_path_name]
-                    group = create_group(as_list_add, node_path_name, node_path_add, group_dict, level+1)
-                    fill_content(node, node_path_add, node_path_name, namespace, group, shacl_dict_add, prefixes_add, meta_data_sub_sub, level+2)
-                else:
-                    if is_required_property(node_properties_add):
-                        # create empty group
-                        node = shacl_dict_add[node_path_add]
-                        create_required_subgraph(node_properties_add, node_path, node_path_name, group_dict, schema_namespace, shacl_dict_add, namespace, prefixes_add, level+1)
-                    #return # not filled in meta_data - ignore
-        return # next node property from original shacl
-    else:   
-        if is_required_property(node_properties):                    
-            node_path_name = get_value('path', node_properties, False)
-            node_path_name = convert_url_to_namespace(node_path_name, True, schema_namespace)
-            group = create_required_subgraph(node_properties, node_path, node_path_name, config.JSON_OUT, schema_namespace, shacl_dict_add, namespace, prefixes_add, level)
-
-      
-def create_required_subgraph(node_properties, node_path, node_path_name, group_dict, schema_namespace, shacl_dict_sub, namespace_sub, prefixes_sub, level):      
-    as_list = is_list_property(node_properties)
-    node_path_name = get_value('path', node_properties, False)
-    node_path_name = convert_url_to_namespace(node_path_name, True, schema_namespace)
-    # add group
-    group = create_group(as_list, node_path_name, node_path, group_dict, level)  
-    # add subgraph
-    if node_path in shacl_dict_sub:
-        schema_shape_add = shacl_dict_sub[node_path]
-        for node_properties_add in schema_shape_add:
-            if is_required_property(node_properties_add):  
-                node_path_add = get_value('node', node_properties_add, False, True)
-                if node_path_add is None:
-                    create_property_old(node_properties_add, prefixes_sub, None, namespace_sub, group, group, level)
-                else:
-                    node_path_name_add = get_value('path', node_properties_add, False)
-                    node_path_name_add = convert_url_to_namespace(node_path_name_add, True, namespace_sub)
-                    create_required_subgraph(node_properties_add, node_path_add, node_path_name_add, group, namespace_sub, shacl_dict_sub, namespace_sub, prefixes_sub, level+1)
-
-    # hack for georeference:GeodeticReferenceSystemShape to add sh:or georeference:coordinateSystemName   
-    if node_path_name == "georeference:geodeticReferenceSystem":
-        if 'georeference:coordinateSystemName' not in group:
-            property = dict()
-            property['@type'] = 'xsd:string'   
-            property['@value'] = ''
-            group['georeference:coordinateSystemName'] = property                
-    return group
-"""
 
 # create property like
 # "hdmap:elevationRange": {
@@ -585,7 +223,7 @@ def create_node(namespace : str, shapename : str, type: str, lsonLD_dict: dict, 
     return node
 
 
-# get shacle
+# get shacle shema
 def get_shacle_shema(namespace : str) -> dict:
     if namespace in config.SHACLS:
         return config.SHACLS[namespace]
@@ -601,6 +239,7 @@ def get_shacle_shape(namespace : str, shapename : str) -> dict:
     
     return None
 
+# register key + value to json ld
 def register_key(key : str, value, meta_data: dict, nodes : list, namespace: str, shapename: str, path: str, is_required: bool, lsonLD_dict: dict, level : int):
     if key in meta_data:
         if isinstance(meta_data[key], list):
@@ -640,6 +279,7 @@ def register_key(key : str, value, meta_data: dict, nodes : list, namespace: str
         # TODO write empty node
         test = 0
 
+# register list of key + value to json ld
 def register_list(key : str, value, meta_data: list, nodes : list, namespace: str, shapename: str, path: str, is_required: bool, lsonLD_dict: dict, level : int):
     if key in meta_data:
         if not isinstance(meta_data[key], list):
@@ -693,50 +333,6 @@ def process_node(shape_value: dict, meta_data: Union[Dict, List], lsonLD_dict: d
             register_list(key, value, meta_data, nodes, namespace, shapename, path, is_required, lsonLD_dict, level)                
         else:
             register_key(key, value, meta_data, nodes, namespace, shapename, path, is_required, lsonLD_dict, level)
-
-
-"""
-def fill_properties(meta_data, schema_namespace, schema_name):
-    shacl_graph_data = config.SHACLS[schema_namespace]
-    prefixes = shacl_graph_data['prefixes']
-    shacl_dict = shacl_graph_data['dict']
-    level = 0
-    
-    # find schema shape
-    schema_node_name = f'{schema_name}shape'.lower()
-    schema_shape = get_value(schema_node_name, shacl_dict, True)
-    if not schema_shape:
-        return
-    
-    # loop properties in shacle
-    for node_properties in schema_shape:
-        path_data, node_paths = get_node_data(node_properties)
-        if node_paths == None:
-            continue # in main shacle groups should be only nodes 
-
-        # if node not in current shacle
-        for node_path in node_paths:
-            if node_path not in shacl_dict:
-                # then switch to specific shacle
-                fill_properties_in_other_namespace(node_properties, node_path, schema_namespace, meta_data, level)
-                continue
-            else: # is in current shacle -> get node 
-                node = shacl_dict[node_path]
-
-            # check if node in meta data
-            node_path_name = get_value('path', node_properties, False)
-            node_path_name = convert_url_to_namespace(node_path_name, True, schema_namespace)
-            if node_path_name in meta_data:
-                # create sub group and fill sub content
-                meta_data_sub = meta_data[node_path_name]
-                as_list = is_list_property(node_properties)
-                group = create_group(as_list, node_path_name, node_path, config.JSON_OUT, level)
-                fill_content(node, node_path, node_path_name, schema_namespace, group, shacl_dict, prefixes, meta_data_sub, level+1)
-            else:
-                if is_required_property(node_properties):
-                    create_required_subgraph(node_properties, node_path, node_path_name, config.JSON_OUT, schema_namespace, shacl_dict, schema_namespace, prefixes, level)
-
-"""
 
 
 # get prefix from url
